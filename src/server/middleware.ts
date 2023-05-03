463200cd65092e5175044406759dcc6977715d34
Nam Hoang Nguyen <nguyenbydoing@gmail.com>
2023-05-03 00:00:57 -0700

    Connect DB, tprc posts call

---
+++ src/server/middleware.ts
@@ -0,0 +1,11 @@
+import { withClerkMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import type { NextRequest } from 'next/server'
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+export default withClerkMiddleware((req: NextRequest) => {
+  return NextResponse.next();
+});
+
+// Stop Middleware running on static files
+export const config = { matcher:  '/((?!_next/image|_next/static|favicon.ico).*)'};
